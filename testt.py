ac1d55bf8b1721940eb0e1d047ee0ce1a5bb3888
thariqakmal23 <154675500+thariqakmal23@users.noreply.github.com>
2023-12-23 19:54:58 +0700

    Update testt.py

--- testt.py
+++ testt.py
@@ -1,17 +1,1 @@
-# SOAL
 
-Sebuah benda jatuh bebas dari ketinggian \(20 \ \text{m}\). Jika percepatan gravitasi bumi adalah \(10 \ \text{m/s}^2\), maka waktu yang diperlukan oleh benda sampai ke tanah adalah?
-
-**Diketahui:**
-\[ h = 20 \ \text{m} \]
-\[ g = 10 \ \text{m/s}^2 \]
-
-**Ditanya:**
-\[ t = ? \]
-
-**Penyelesaian:**
-\[ h = \frac{1}{2} g t^2 \]
-\[ 20 \ \text{m} = \frac{1}{2} \times 10 \ \text{m/s}^2 \times t^2 \]
-\[ t^2 = \frac{20}{5} \ \text{s}^2 \]
-\[ t^2 = \sqrt{4} \]
-\[ t = 2 \ \text{s} \]
